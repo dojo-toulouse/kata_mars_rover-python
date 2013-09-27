c9caa30f08ea5000a82d23d98f5ad2f3b569a3c3
Antoine Cezar <antoine.cezar@gmail.com>
2013-09-27 08:38:05 +0200

    Add session's code

--- mars_rover.py
+++ mars_rover.py
@@ -1,2 +1,52 @@
 class MarsRover(object):
-    pass
+
+    cardinal_points = ["N", "E", "S", "W"]
+
+    def __init__(self):
+        self.cardinal_point_index = 0
+        self.x = 0
+        self.y = 0
+
+    def get_position(self):
+        return (self.x,
+                self.y,
+                self.cardinal_points[self.cardinal_point_index])
+
+    def move(self, commands):
+        for command in commands:
+            self.execute(command)
+
+    def execute(self, command):
+        if command == 'f':
+            self.move_forward()
+        elif command == 'r':
+            self.turn_right()
+        elif command == 'l':
+            self.turn_left()
+
+    def move_forward(self):
+        if self.is_facing_north():
+            self.y += 1
+        elif self.is_facing_east():
+            self.x += 1
+        else:
+            self.x -= 1
+
+    def is_facing_north(self):
+        return self.is_facing('N')
+
+    def is_facing_east(self):
+        return self.is_facing('E')
+
+    def is_facing(self, direction):
+        return self.cardinal_points.index(direction) == self.cardinal_point_index
+
+    def turn(self, increment):
+        self.cardinal_point_index += increment
+        self.cardinal_point_index %= len(self.cardinal_points)
+
+    def turn_right(self):
+        self.turn(1)
+
+    def turn_left(self):
+        self.turn(-1)
--- test_mars_rover.py
+++ test_mars_rover.py
@@ -6,5 +6,59 @@ class TestMarsRover(TestCase):
     def setUp(self):
         self.rover = MarsRover()
 
-    def test_exemple(self):
-        self.assertIsInstance(self.rover, MarsRover)
+    def test_init_position(self):
+        self.assertEquals(self.rover.get_position(), (0, 0, 'N'))
+
+    def test_move_forward_from_init_pos(self):
+        self.rover.move('f')
+        self.assertEquals(self.rover.get_position(), (0, 1, 'N'))
+
+    def test_rotate_right_from_init_pos(self):
+        self.rover.move('r')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'E'))
+
+    def test_rotate_right_twice_from_init_pos(self):
+        self.rover.move('r')
+        self.rover.move('r')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'S'))
+
+    def test_rotate_three_times_from_init_pos(self):
+        self.rover.move('r')
+        self.rover.move('r')
+        self.rover.move('r')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'W'))
+
+    def test_rotate_four_times_from_init_pos(self):
+        self.rover.move('r')
+        self.rover.move('r')
+        self.rover.move('r')
+        self.rover.move('r')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'N'))
+
+    def test_rotate_left_from_init_pos(self):
+        self.rover.move('l')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'W'))
+
+    def test_rotate_left_and_then_move_forward(self):
+        self.rover.move('l')
+        self.rover.move('f')
+        self.assertEquals(self.rover.get_position(), (-1, 0, 'W'))
+
+    def test_rotate_left_five_times_from_init_pos(self):
+        self.rover.move('l')
+        self.rover.move('l')
+        self.rover.move('l')
+        self.rover.move('l')
+        self.rover.move('l')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'W'))
+
+    def test_rover_accepts_multiple_commands(self):
+        self.rover.move('rr')
+        self.assertEquals(self.rover.get_position(), (0, 0, 'S'))
+
+    def test_move_forward_facing_east(self):
+        self.rover.move('r')
+
+        self.rover.move('f')
+
+        self.assertEquals(self.rover.get_position(), (1, 0, 'E'))
